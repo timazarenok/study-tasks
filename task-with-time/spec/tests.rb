03db0cf48a85d7a2ccc17a762b76aa24928e7082
timazarenok <timofey.zarenok@gmail.com>
2020-10-16 15:53:54 +0300

    add rubocop time-task

--- task-with-time/spec/tests.rb
+++ task-with-time/spec/tests.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rspec/autorun'
 require '../lib/time.rb'
 
 describe Intervals do
   it 'without intersections' do
     times = Intervals.new([['10:00', '10:20'], ['10:40', '11:00'], ['11:50', '12:00'], ['12:10', '13:00'], ['10:00', '10:20']])
-    expect(times.free_time).to eq([[ '10:20', '10:40'], ['11:00', '11:50'], ['12:00', '12:10']])
+    expect(times.free_time).to eq([['10:20', '10:40'], ['11:00', '11:50'], ['12:00', '12:10']])
   end
 
   it 'with one intersection' do
@@ -31,5 +33,4 @@ describe Intervals do
     times = Intervals.new([['10:00', '10:20'], ['12:00', '13:00'], ['10:40', '11:00'], ['11:00', '12:00'], ['13:30', '18:20']])
     expect(times.free_time).to eq([['10:20', '10:40'], ['13:00', '13:30']])
   end
-
 end

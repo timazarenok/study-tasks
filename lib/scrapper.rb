a45f07b551650661f5e3b440ab24c8adad826710
timazarenok <timofey.zarenok@gmail.com>
2020-10-19 18:32:08 +0300

    add scrapper files

---
+++ lib/scrapper.rb
@@ -0,0 +1,20 @@
+require './info.rb'
+require 'capybara'
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.javascript_driver = :chrome
+Capybara.configure do |config|
+  config.default_max_wait_time = 10
+  config.default_driver = :selenium
+end
+
+URL = 'https://www.onliner.by/news'
+
+browser = Capybara.current_session
+driver = browser.driver.browser
+browser.visit URL
+
+Info.new(driver).get_names 

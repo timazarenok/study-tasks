c9902de05379ea874e7c53cfe954c9b16006bd43
timazarenok <timofey.zarenok@gmail.com>
2020-10-20 16:18:56 +0300

    fix special section

--- lib/info.rb
+++ lib/info.rb
@@ -8,7 +8,7 @@ class Info
 
   def get_data
     @source.xpath('//*[contains(@class,"b-tile m-1x1 m-info")]').each do |row|
-      name = row.css('[class="b-tile-section"]').text
+      name = row.css('[class*="b-tile-section"]').text
       text = row.css('[class="txt max-lines-4"]').text
       image = row.css('[class="b-tile-bg"]').first['style']
       @data.push(name: name, text: text, image: image)

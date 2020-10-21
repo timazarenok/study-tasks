4dde5e44c81cc02381313ec5395e9d3aaafb78b8
timazarenok <timofey.zarenok@gmail.com>
2020-10-21 14:54:30 +0300

    chec push request

--- lib/info.rb
+++ lib/info.rb
@@ -14,6 +14,7 @@ class Info
       image = row.css('[class="b-tile-bg"]').first['style']
       id = get_id(row)
       text = get_text(id)
+      
       @browser.go_back
       @data.push(name: name, text: text, image: image)
     end
